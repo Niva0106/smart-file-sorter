745fc0603c10625b52fe6f6478e0a4f8c3ccdffc
Niveditha <nivedithamayannur@gmail.com>
2026-05-24 23:13:58 +0530

    Updated scanner logic

---
+++ utils/scanner.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+def scan_downloads():
+    downloads = Path.home() / "Downloads"
+
+    if not downloads.exists():
+        return []
+
+    files = []
+
+    for item in downloads.iterdir():
+        if item.is_file():
+            files.append(item)
+
+    return files
