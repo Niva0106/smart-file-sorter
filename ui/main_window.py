f801b1903095e349ff75bbad0cd03c2cfd58b0ca
Niveditha <nivedithamayannur@gmail.com>
2026-05-25 00:49:01 +0530

    Connected the main logic to mover

--- ui/main_window.py
+++ ui/main_window.py
@@ -2,6 +2,8 @@ from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QListWidget, QL
 from utils.scanner import scan_downloads
 from utils.file_info import get_file_info
 from utils.category_manager import get_category
+from utils.mover import move_file
+from pathlib import Path
 
 
 class MainWindow(QWidget):
@@ -21,10 +23,15 @@ class MainWindow(QWidget):
 
         self.file_list = QListWidget()
 
+        self.sort_button = QPushButton("Sort Files")
+        self.sort_button.clicked.connect(self.sort_files)
+
         self.layout.addWidget(self.title)
         self.layout.addWidget(self.subtitle)
         self.layout.addWidget(self.scan_button)
         self.layout.addWidget(self.file_list)
+        self.layout.addWidget(self.sort_button)
+
 
         self.setLayout(self.layout)
     
@@ -38,3 +45,13 @@ class MainWindow(QWidget):
             category = get_category(file)
 
             self.file_list.addItem(f"{info['name']} → {category}")
+
+    def sort_files(self):
+        files = scan_downloads()
+        destination = Path.home() / "Downloads"
+
+        for file in files:
+            category = get_category(file)
+            move_file(file, category, destination)
+
+        self.load_files()
